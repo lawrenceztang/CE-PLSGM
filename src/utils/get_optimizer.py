0a45f9dde05637a02d28a14ce32abd77ea390229
lawrencetang <lawrenceztang@gmail.com>
2024-02-03 19:42:41 -0600

    Fix name

--- src/utils/get_optimizer.py
+++ src/utils/get_optimizer.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append('../')
 
-from optimizers import Diff2_GD_Server, CE_PLSGM_Server
+from optimizers import Diff2_GD_Server, CE_PSGM_Server
 
 
 def get_optimizer(optimizer_name, **kargs):
     optimizers = {"diff2_gd": Diff2_GD_Server, #Diff2 with GD-Routine
-                  "ce_plsgm": CE_PLSGM_Server
+                  "ce_plsgm": CE_PSGM_Server
                   }
     return optimizers[optimizer_name](**kargs)
